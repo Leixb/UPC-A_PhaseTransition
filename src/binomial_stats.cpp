2f8a8cdfe87d84f4f35e229d1303ee314017c619
Leixb <abone9999@gmail.com>
2019-09-23 21:49:09 +0200

    Enter rep and delta as parameters

--- src/binomial_stats.cpp
+++ src/binomial_stats.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
+#include <cstdlib>
 
 #include "graph.h"
 #include "graph_generator.h"
 
 using namespace std;
 
-#define DELTA_P 0.0025
-#define N_CASOS 50
+void usage() {
+	cerr << "USAGE:" << endl
+		 << "./prog N repeticions delta_p" << endl;
+}
 
 int main(int argc, char *argv[]) {
 
-	if (argc != 2) {
-		cerr << "Give N as parameter" << endl;
+	seed_generator();
+
+	if (argc != 4) {
+		usage();
 		return 1;
 	}
 
-	seed_generator();
-
 	const int n = atoi(argv[1]);
+	const int repeticions = atoi(argv[2]);
+	const double delta_p = stod(argv[3]);
 
-	cout << "# p-connected" << ' ' << "p" << endl;
+	cout << "# N=" << n << " REP=" << repeticions << " delta=" << delta_p << endl
+		 << "# p-connected" << ' ' << "p" << endl;
 
-	for (double p = 0.0; p <= 1.0; p += DELTA_P) {
+	for (double p = 0.0; p <= 1.0; p += delta_p) {
 
 		int count = 0;
 
-		for (int i = 0; i < N_CASOS; ++i) {
+		for (int i = 0; i < repeticions; ++i) {
 			count += BRG(n, p).NconnectedComponents() == 1;
 		}
 
-		cout << p << ' ' << double(count)/double(N_CASOS) << endl;
+		cout << p << ' ' << double(count)/double(repeticions) << endl;
 
 	}
 
