cdcd9f272d33ab1a4bfb7b1d115d090fc1d1bc51
Leixb <abone9999@gmail.com>
2019-09-24 20:30:11 +0200

    Added xlabel and ylabel flags

--- calc_and_plot.py
+++ calc_and_plot.py
@@ -11,12 +11,14 @@ import matplotlib.pyplot as plt
 
 import argparse
 
-def compute_stats(prog, n, rep, dp, out_dir):
+def compute_stats(n):
 
-    output_filename = '{}/{}_{:04d}.dat'.format(out_dir, os.path.basename(prog), n)
+    output_filename = '{}/{}_{:04d}.dat'.format(args.out_dir, os.path.basename(args.program), n)
+
+    print(output_filename)
 
     with open(output_filename, 'w+') as output:
-        subprocess.call((prog, str(n), str(rep), str(dp)), stdout=output)
+        subprocess.call((args.program, str(n), str(args.repetitions), str(args.delta_p)), stdout=output)
 
     return output_filename
 
@@ -33,10 +35,10 @@ def plot(filename):
     #plt.plot(x, y)
 
     plt.title(os.path.basename(filename))
-    plt.xlabel('p')
-    plt.ylabel('Probability that the network is connected')
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
+    plt.xlabel(args.xlabel)
+    plt.ylabel(args.ylabel)
+    #plt.xlim(0, 1)
+    #plt.ylim(0, 1)
 
     plt.savefig(plot_filename)
     plt.close()
@@ -49,9 +51,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('.')
     parser.add_argument('program')
     parser.add_argument('--repetitions', '-r', type=int, default=50)
-    parser.add_argument('--delta_p', '-d', default=0.01, type=float)
+    parser.add_argument('--delta-p', '-d', default=0.01, type=float)
     parser.add_argument('N', type=int, nargs='+')
     parser.add_argument('--out-dir', '-o')
+    parser.add_argument('--ylabel', '-y', type=str, default='')
+    parser.add_argument('--xlabel', '-x', type=str, default='')
 
     args = parser.parse_args()
 
@@ -60,8 +64,7 @@ if __name__ == '__main__':
 
     def compute_stats_wrapped(n):
         print('Started   N=', n)
-        filename = compute_stats(args.program, n, args.repetitions, args.delta_p,
-                      args.out_dir)
+        filename = compute_stats(n)
         print('Finished  N=', n)
 
         return filename
@@ -70,8 +73,8 @@ if __name__ == '__main__':
 
         results = []
 
-        for n in args.N:
-            r = pool.apply_async(compute_stats_wrapped, (n,), callback=plot)
+        for nn in args.N:
+            r = pool.apply_async(compute_stats_wrapped, (nn,), callback=plot)
             results.append(r)
 
         for r in results:
