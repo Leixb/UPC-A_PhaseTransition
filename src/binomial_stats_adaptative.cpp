d5c7f8c00f99cf6884641d1a386b910eaf3903e9
Leixb <abone9999@gmail.com>
2019-09-24 12:47:46 +0200

    Utilitza llistes per evitar redundancies

--- src/binomial_stats_adaptative.cpp
+++ src/binomial_stats_adaptative.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <list>
 
 #include "graph.h"
 #include "graph_generator.h"
@@ -23,30 +24,29 @@ double average(const int& n, const double &p, const int &rep) {
 	return double(repeat(n, p, rep))/double(rep);
 }
 
-void calculate(const vector<double> &v, const int &n, const double &dy,
-			   const int&rep, const int &depth=0) {
-	if (depth >= DEPTH_LIMIT) return;
-	vector <double> next_it;
-	vector <double> res (v.size());
+void calculate(list<pair<double, double>> &l, vector<list<pair<double,double> >::iterator> &v,
+			   const int &n, const double &dy, const int&rep, const int &depth=0) {
+	if (depth > DEPTH_LIMIT) return; // no s hauria d arribar a complir mai si no afegim a next_it a la ultima iteracio
 
-	res[0] = average(n, v[0], rep);
-	cout << v[0] << ' ' << res[0] << endl;
+	vector <list<pair<double, double> >::iterator> next_it;
 
-	// p = v[i]
+	for (const auto it : v) {
+		auto prev = it;
+		advance(prev, -1);
 
-	for (size_t i = 1; i < v.size(); ++i) {
-		res[i] = average(n, v[i], rep);
+		if (it->second < -0.5) // Nomes calculem els que no han estat calculats previament
+			it->second = average(n, it->first, rep);
 
-		cout << v[i] << ' ' << res[i] << endl;
+		if (depth == DEPTH_LIMIT) continue; // No afegim mes si ja estem al limit
 
-		if (abs(res[i] - res[i-1]) > dy) {
-			next_it.push_back(v[i-1]);
-			next_it.push_back((v[i]+v[i-1])/2);
-			next_it.push_back(v[i]);
+		if (abs(it->second - prev->second) > dy) {
+			next_it.push_back(l.emplace(it, (it->first + prev->first)/2, -1));
+			next_it.push_back(it);
 		}
 	}
 
-	if (next_it.size() > 0) calculate(next_it, n, dy, rep, depth+1);
+	if (next_it.size() > 0)
+		calculate(l, next_it, n, dy, rep, depth+1);
 }
 
 int main(int argc, char *argv[]) {
@@ -65,12 +65,21 @@ int main(int argc, char *argv[]) {
 	cout << "# N=" << n << " REP=" << repeticions << " dy=" << dy << endl
 		 << "# p-connected" << ' ' << "p" << endl;
 
-	vector<double> v(10);
-	for (size_t i = 0; i < v.size(); ++i)
-		v[i] = double(i)/double(v.size());
+	const size_t part_ini = 10;
 
-	calculate(v, n, dy, repeticions);
-	//calculate({0,1}, n, dy, repeticions);
+	list<pair<double, double> > l;
+	l.emplace_back(0, 0);
+
+	vector<list<pair<double, double> >::iterator> v(part_ini-1);
+
+	for (size_t i = 1; i < part_ini; ++i)
+		v[i-1] = l.emplace(l.end(), double(i)/double(part_ini), -1);
+
+
+	calculate(l, v, n, dy, repeticions);
+	for (const pair<double, double>& p : l)
+		//if (p.second > -0.5)
+		cout << p.first << ' ' << p.second << endl;
 
 	return 0;
 }
