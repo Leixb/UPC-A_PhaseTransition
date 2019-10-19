3a1a91e7b2c6e300625272203fb8fea9e463b12d
Leixb <abone9999@gmail.com>
2019-10-19 22:40:14 +0200

    Clean up makePlots

--- makePlots.sh
+++ makePlots.sh
@@ -25,8 +25,16 @@ function plot() {
 }
 
 function plot_mult() {
+	NAME="$1_$2"
+	OUTPUT="${PLOT_DIR}/${NAME}.$EXT"
+	echo "Ploting : $OUTPUT ($TITLE)"
 	python3 plot.py -t "$TITLE" -x "$XLABEL" -y "$YLABEL" --xmin $XMIN --xmax $XMAX \
-		-o "$OUTPUT" --show-legend $@ 2>/dev/null
+		-o "$OUTPUT" --show-legend "${FOLDER}/${NAME}.dat" 2>/dev/null
+}
+
+function plot_mult_bingeo() {
+	TITLE="${TITLE}${TITLE_GRG}" 	XLABEL=r plot_mult GRG $@
+	TITLE="${TITLE}${TITLE_BRG}" 	XLABEL=p plot_mult BRG $@
 }
 
 # 1: datafile
@@ -55,44 +63,46 @@ mkdir -p "$PLOT_DIR"
 
 VALORS_N="$*"
 
-DY=0.10 compute GRG numCompCon	 	| YLABEL="Avg. Mida component connexa gegant" 	XLABEL=r pipe "Components conexes en Geometric Random Graph"
-DY=0.01 compute GRG esConnex	 	| YLABEL="Probabilitat de ser connex" 			XLABEL=r pipe "Connectivitat en Geometric Random Graph"
-DY=0.10 compute GRG midaCompConMax 	| YLABEL="Avg. Mida component connexa gegant" 	XLABEL=r pipe "Mida component conexa gegant en Geometric Random Graph"
-DY=0.01 compute GRG cicle 			| YLABEL="Probabilitat de tenir cicle"		    XLABEL=r pipe "Cicles en Geometric Random Graph"
-DY=0.01 compute GRG eulerianPath 	| YLABEL="Probabilitat de tenir camí eulerià"	XLABEL=r pipe "Camí eulerià en Geometric Random Graph"
-DY=0.01 compute GRG eulerianCycle 	| YLABEL="Probabilitat de tenir cicle eulerià"	XLABEL=r pipe "Cicle eulerià en Geometric Random Graph"
+TITLEeulerianCycle="Cicle eulerià"
+TITLEeulerianPath="Camí eulerià"
+TITLEcicle="Cicles"
+TITLEmidaCompConMax="Mida de la component conexa gegant"
+TITLEesConnex="Connectivitat"
+TITLEnumCompCon="Components conexes"
 
-DY=0.10 compute BRG numCompCon	 	| YLABEL="Avg. Mida component connexa gegant" 	XLABEL=p pipe "Components conexes en Binomial Random Graph"
-DY=0.01 compute BRG esConnex	 	| YLABEL="Probabilitat de ser connex" 			XLABEL=p pipe "Connectivitat en Binomial Random Graph"
-DY=0.10 compute BRG midaCompConMax 	| YLABEL="Avg. Mida component connexa gegant" 	XLABEL=p pipe "Mida Component conexa gegant en Binomial Random Graph"
-DY=0.01 compute BRG cicle 			| YLABEL="Probabilitat de tenir cicle"		 	XLABEL=p pipe "Cicles en Binomial Random Graph"
-DY=0.01 compute BRG eulerianPath 	| YLABEL="Probabilitat de tenir camí eulerià" 	XLABEL=p pipe "Camí eulerià en Binomial Random Graph"
-DY=0.01 compute BRG eulerianCycle 	| YLABEL="Probabilitat de tenir cicle eulerià" 	XLABEL=p pipe "Cicle eulerià en Binomial Random Graph"
+TITLE_GRG=" en Geometric Random Graph"
+TITLE_BRG=" en Binomial Random Graph"
 
-OUTPUT="${PLOT_DIR}/GRG_numCompCon.$EXT" 		YLABEL="Avg. Mida component connexa gegant" 	XLABEL=r TITLE="Components conexes en Geometric Random Graph" \
-	plot_mult "${FOLDER}/GRG_numCompCon*.dat"
-OUTPUT="${PLOT_DIR}/GRG_esConnex.$EXT" 		YLABEL="Probabilitat de ser connex" 				XLABEL=r TITLE="Connectivitat en Geometric Random Graph" \
-	plot_mult "${FOLDER}/GRG_esConnex*.dat"
-OUTPUT="${PLOT_DIR}/GRG_midaCompConMax.$EXT" 	YLABEL="Avg. Mida component connexa gegant" 	XLABEL=r TITLE="Mida component conexa gegant en Geometric Random Graph" \
-	plot_mult		 "${FOLDER}/GRG_midaCompConMax*.dat"
-OUTPUT="${PLOT_DIR}/GRG_cicle.$EXT" 			YLABEL="Probabilitat de tenir cicle"		    XLABEL=r TITLE="Cicles en Geometric Random Graph" \
-	plot_mult		 "${FOLDER}/GRG_cicle*.dat"
-OUTPUT="${PLOT_DIR}/GRG_eulerianPath.$EXT" 		YLABEL="Probabilitat de tenir camí eulerià"		XLABEL=r TITLE="Camí eulerià en Geometric Random Graph" \
-	plot_mult		 "${FOLDER}/GRG_eulerianPath*.dat"
-OUTPUT="${PLOT_DIR}/GRG_eulerianCycle.$EXT" 	YLABEL="Probabilitat de tenir cicle eulerià"	XLABEL=r TITLE="Cicle eulerià en Geometric Random Graph" \
-	plot_mult		 "${FOLDER}/GRG_eulerianCycle*.dat"
+LABELeulerianCycle="Probabilitat de contenir un cicle eulerià"
+LABELeulerianPath="Probabilitat de contenir un camí eulerià"
+LABELcicle="Probabilitat de contenir un cicle"
+LABELmidaCompConMax="Mida esperada de la component connexa gegant"
+LABELesConnex="Probabilitat de ser connex"
+LABELnumCompCon="Nombre de components connexos esperats"
 
-OUTPUT="${PLOT_DIR}/BRG_numCompCon.$EXT" 		YLABEL="Avg. Mida component connexa gegant" 	XLABEL=p TITLE="Components conexes en Binomial Random Graph" \
-	plot_mult		 "${FOLDER}/BRG_numCompCon*.dat"
-OUTPUT="${PLOT_DIR}/BRG_esConnex.$EXT" 		YLABEL="Probabilitat de ser connex" 				XLABEL=p TITLE="ponnectivitat en Binomial Random Graph" \
-	plot_mult "${FOLDER}/BRG_esConnex*.dat"
-OUTPUT="${PLOT_DIR}/BRG_midaCompConMax.$EXT" 	YLABEL="Avg. Mida component connexa gegant" 	XLABEL=p TITLE="Mida Component conexa gegant en Binomial Random Graph" \
-	plot_mult		 "${FOLDER}/BRG_midaCompConMax*.dat"
-OUTPUT="${PLOT_DIR}/BRG_cicle.$EXT" 			YLABEL="Probabilitat de tenir cicle"		 	XLABEL=p TITLE="Cicles en Binomial Random Graph" \
-	plot_mult		 "${FOLDER}/BRG_cicle*.dat"
-OUTPUT="${PLOT_DIR}/BRG_eulerianPath.$EXT" 		YLABEL="Probabilitat de tenir camí eulerià" 	XLABEL=p TITLE="Camí eulerià en Binomial Random Graph" \
-	plot_mult		 "${FOLDER}/BRG_eulerianPath*.dat"
-OUTPUT="${PLOT_DIR}/BRG_eulerianCycle.$EXT" 	YLABEL="Probabilitat de tenir cicle eulerià" 	XLABEL=p TITLE="Cicle eulerià en Binomial Random Graph" \
-	plot_mult		 "${FOLDER}/BRG_eulerianCycle*.dat"
+if [ $# -ne 0 ]; then
+	XLABEL=r
+	DY=0.10 compute GRG numCompCon	 		| YLABEL="$LABELnumCompCon" 		pipe "${TITLEnumCompCon}${TITLE_GRG}"
+	DY=0.01 compute GRG esConnex 			| YLABEL="$LABELesConnex"			pipe "${TITLEesConnex}${TITLE_GRG}"
+	DY=0.10 compute GRG midaCompConMax 		| YLABEL="$LABELmidaCompConMax"		pipe "${TITLEmidaCompConMax}${TITLE_GRG}"
+	DY=0.01 compute GRG cicle 				| YLABEL="$LABELcicle"				pipe "${TITLEcicle}${TITLE_GRG}"
+	DY=0.01 compute GRG eulerianPath 		| YLABEL="$LABELeulerianPath"		pipe "${TITLEeulerianPath}${TITLE_GRG}"
+	DY=0.01 compute GRG eulerianCycle 		| YLABEL="$LABELeulerianCycle"		pipe "${TITLEeulerianCycle}${TITLE_GRG}"
+
+	XLABEL=p
+	DY=0.10 compute BRG numCompCon	 	 	| YLABEL="$LABELnumCompCon"			pipe "${TITLEnumCompCon}${TITLE_BRG}"
+	DY=0.01 compute BRG esConnex	 	 	| YLABEL="$LABELesConnex"			pipe "${TITLEesConnex}${TITLE_BRG}"
+	DY=0.10 compute BRG midaCompConMax 	 	| YLABEL="$LABELmidaCompConMax"		pipe "${TITLEmidaCompConMax}${TITLE_BRG}"
+	DY=0.01 compute BRG cicle 			 	| YLABEL="$LABELcicl"				pipe "${TITLEcicle}${TITLE_BRG}"
+	DY=0.01 compute BRG eulerianPath 	 	| YLABEL="$LABELeulerianPath"		pipe "${TITLEeulerianPath}${TITLE_BRG}"
+	DY=0.01 compute BRG eulerianCycle 	 	| YLABEL="$LABELeulerianCycle"		pipe "${TITLEeulerianCycle}${TITLE_BRG}"
+fi
+
+YLABEL="$LABELnumCompCon" 		TITLE="$TITLEnumCompCon" 		plot_mult_bingeo numCompCon
+YLABEL="$LABELesConnex"			TITLE="$TITLEesConnex" 			plot_mult_bingeo esConnex
+YLABEL="$LABELmidaCompConMax" 	TITLE="$TITLEmidaCompConMax" 	plot_mult_bingeo midaCompConMax
+YLABEL="$LABELcicle"		    TITLE="$TITLEcicle" 			plot_mult_bingeo cicle
+YLABEL="$LABELeulerianPath"		TITLE="$TITLEeulerianPath" 		plot_mult_bingeo eulerianPath
+YLABEL="$LABELeulerianCycle"	TITLE="$TITLEeulerianCycle" 	plot_mult_bingeo eulerianCycle
 
 wait
